723bd1119a699b189da5c9ff653ccbaa16d209f6
leonardossev <leonardossev@gmail.com>
2020-03-25 01:58:41 -0300

    chore(db): adiciona conexão com mongodb

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes');
+const mongoose = require('mongoose');
+const connectionString = require('./config/databaseConfig').connectionString;
 
 const port = process.env.APP_PORT | 3000;
+
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 app.use(express.json());
 app.use(routes);

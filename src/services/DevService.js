d2195e68bb1830c3441823bae2fefdddd5d9ab2b
leonardossev <leonardossev@gmail.com>
2020-03-25 02:57:26 -0300

    feat(services): adiciona cadastramento de dev
    Adiciona método responsável por cadastrar um novo desenvolvedor. O
    método verifica se já existe algum usuário com o mesmo username do
    github. Com base nesse username, a aplicação buscará pelo dev na base de
    dados. Se o dev existir, a aplicação o retornará na response. Se não,
    irá manipular os dados do desenvolvedor e cadastrá-lo na base de dados.

--- src/services/DevService.js
+++ src/services/DevService.js
@@ -1,7 +1,29 @@
 const DevRepository = require('../repositories/DevRepository');
+const GithubService = require('./GithubService');
+
+async function performDevStoring({ github_username, latitude, longitude, techs }) {
+  const { name = login, bio, avatar_url } = await GithubService.findUser(github_username);
+
+  const location = {
+    type: 'Point',
+    coordinates: [longitude, latitude]
+  };
+
+  return await DevRepository.store({ github_username, name, bio, avatar_url, location, techs });
+};
 
 module.exports = {
   async findAll() {
     return await DevRepository.findAll();
+  },
+
+  async store({ github_username, latitude, longitude, techs }) {
+    let dev = await DevRepository.findOneByGithubUsername(github_username);
+
+    if (!dev) {
+      dev = await performDevStoring({ github_username, latitude, longitude, techs });
+    }
+
+    return dev;
   }
 };

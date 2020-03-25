a455b12bcc450a49de2760a7758c30e2dc281e51
leonardossev <leonardossev@gmail.com>
2020-03-25 02:59:40 -0300

    feat(repositories): adiciona findOne() e store()
    Adiciona método findOneByGithubUsername que é responsável por buscar um
    desenvolvedor com o um determinado username do Github na base de dados.
    
    Adiciona método que é responsável por cadastrar um novo desenvolvedor na
    base de dados.

--- src/repositories/DevRepository.js
+++ src/repositories/DevRepository.js
@@ -3,5 +3,24 @@ const Dev = require('../models/Dev');
 module.exports = {
   async findAll() {
     return Dev.find();    
+  },
+
+  async findOneByGithubUsername(githubHusername) {
+    return Dev.findOne({
+      github_username: {
+        $eq: githubHusername
+      }
+    });
+  },
+
+  async store({ github_username, name, bio, avatar_url, location, techs }) {
+    return await Dev.create({
+      github_username,
+      name,
+      avatar_url,
+      bio,
+      techs,
+      location
+    });
   }
 };
